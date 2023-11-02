78882570ef79f59256cd303a8997e1e108ee9f18
Yusuf-R <y.abdulwasiu@gmail.com>
2023-11-02 08:56:09 +0100

    feat: :sparkles: it has to be 5 lines

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,7 +5,8 @@ from typing import List
 import logging
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
     """Obfuscates sensitive data in a log message."""
     for f in fields:
         message = re.sub("{}=.*?{}".format(f, separator),
