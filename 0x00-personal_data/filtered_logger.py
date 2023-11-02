ffedf5daf6f480b204dd64c50c5a01d7d2888400
Yusuf-R <y.abdulwasiu@gmail.com>
2023-11-02 04:24:20 +0100

    feat: :sparkles: regex mode activated

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,9 +4,14 @@ import re
 from typing import List
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator) -> str:
+def filter_datum(
+    fields: List[str], redaction: str, message: str, separator
+) -> str:
     """Obfuscates sensitive data in a log message."""
-    d = message
     for f in fields:
-        d = re.sub(rf'{f}=[^;]+', f'{f}={redaction}', d)
-    return d 
+        message = re.sub(
+            "{}=.*?{}".format(f, separator),
+            "{}={}{}".format(f, redaction, separator),
+            message,
+        )
+    return message
