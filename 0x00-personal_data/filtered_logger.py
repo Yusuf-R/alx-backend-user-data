c93aae0721604acd177007af83c3f19dddb2ab2c
Yusuf-R <y.abdulwasiu@gmail.com>
2023-11-02 02:19:26 +0100

    fix: :bug: task base compliance

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,38 +1,10 @@
 #!/usr/bin/env python3
 """Personal Data Template"""
-
 import re
-import logging
-
-# Write a function called filter_datum that returns the log message obfuscated:
-# Arguments:
-# fields: a list of strings representing all fields to obfuscate
-# redaction: a string representing by what the field will be obfuscated
-# message: a string representing the log line
-# separator: a string representing by which character is separating all
-# fields in the log line (message)
-# The function should use a regex to replace occurrences of certain field val.
-# filter_datum should be less than 5 lines long and use re.sub to perform
-# the substitution with a single regex.
 
 
 def filter_datum(fields, redaction, message, separator):
-    """
-    Obfuscates sensitive data in a log message.
-
-    Args:
-    - fields (list of str): the names of the sensitive fields to obfuscate.
-    - redaction (str): the string to use as a replacement for sensitive data.
-    - message (str): the log message to obfuscate.
-    - separator (str): the string to append at the end of the obfuscated mesg.
-
-    Returns:
-    - str: the obfuscated log message, with sensitive fields
-      replaced by the redaction string.
-    """
-    for field in fields:
-        data = re.sub(
-            r"{}=\S+".format(field), "{}={}".format(field, redaction), message
-        )
-    data += separator
-    return data
+    """Obfuscates sensitive data in a log message."""
+    for f in fields:
+        d = re.sub(r"{}=\S+".format(f), "{}={}".format(f, redaction), message)
+    return d + separator
