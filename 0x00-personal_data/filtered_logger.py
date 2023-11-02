91ba74f45357b7a5cb76e96bda3d2c54843d49d7
Yusuf-R <y.abdulwasiu@gmail.com>
2023-11-02 13:30:08 +0100

    feat: :sparkles: create logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,6 +5,25 @@ from typing import List
 import logging
 
 
+# ==========================Task2====================================
+# Implement a get_logger function that takes no arguments and
+# returns a logging.Logger object.
+# The logger should be named "user_data" and only log up to logging.INFO level.
+# It should not propagate messages to other loggers.
+# It should have a StreamHandler with RedactingFormatter as formatter.
+# Create a tuple PII_FIELDS constant at the root of the module containing
+# the fields from user_data.csv that are considered PII.
+# PII_FIELDS can contain only 5 fields - choose the right list of fields that
+# can are considered as “important” PIIs or information that you must hide in
+# your logs.
+# Use it to parameterize the formatter.
+# =====================================================================
+
+
+# Create a tuple PII_FIELDS constant at the root of the module containing
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
+
+
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
     """Obfuscates sensitive data in a log message."""
@@ -29,3 +48,22 @@ class RedactingFormatter(logging.Formatter):
         return (filter_datum(self.fields, self.REDACTION,
                              super(RedactingFormatter, self).format(record),
                              self.SEPARATOR))
+
+
+def get_logger() -> logging.Logger:
+    """Retur a logging.Logger object"""
+    # logger = logging.getLogger('user_data')
+    # logger.setLevel(logging.INFO)
+    # logger.propagate = False
+    # handler = logging.StreamHandler()
+    # handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    # logger.addHandler(handler)
+    # return logger
+    # OR :
+    logger = logging.Logger(name="user_data")
+    logger.setLevel = logging.INFO
+    logger.propagate = False
+    handler = logging.StreamHandler()
+    handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.addHandler(handler)
+    return logger
