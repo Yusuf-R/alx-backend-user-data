1cdb922f3700ae15ac29265e4e8136e8e1a8375d
Yusuf-R <y.abdulwasiu@gmail.com>
2023-11-02 08:44:50 +0100

    feat: :sparkles: trying to meet 5 lines requirement

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,16 +2,25 @@
 """Personal Data Template"""
 import re
 from typing import List
+import logging
 
 
-def filter_datum(
-    fields: List[str], redaction: str, message: str, separator
-) -> str:
+def filter_datum(fields, redaction, message, separator):
     """Obfuscates sensitive data in a log message."""
     for f in fields:
-        message = re.sub(
-            "{}=.*?{}".format(f, separator),
-            "{}={}{}".format(f, redaction, separator),
-            message,
-        )
+        message = re.sub("{}=.*?{}".format(f, separator),
+                         "{}={}{}".format(f, redaction, separator), message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class"""
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        NotImplementedError
