31bf19804d35bcf919e04cc30e54d7ea7f2a146a
Yusuf-R <y.abdulwasiu@gmail.com>
2023-11-02 02:31:54 +0100

    fix: :bug: added type annotation

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 """Personal Data Template"""
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator) -> str:
     """Obfuscates sensitive data in a log message."""
     for f in fields:
         d = re.sub(r"{}=\S+".format(f), "{}={}".format(f, redaction), message)
