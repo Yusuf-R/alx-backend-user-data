b3b994432dd9dfee90d6fdd3f29874893551ae03
Yusuf-R <y.abdulwasiu@gmail.com>
2023-11-07 10:03:39 +0100

    feat: :sparkles: update require_auth method to ensure authentication for some route outside excluded path

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -4,15 +4,45 @@
 from flask import request
 from typing import List, TypeVar
 from os import getenv
+import re
 
+
+# =============== require_auth ===============
+# Returns True if path is None
+# Returns True if excluded_paths is None or empty
+# Returns False if path is in excluded_paths
+# You can assume excluded_paths contains string path always ending by a /
+# This method must be slash tolerant:
+# path=/api/v1/status and path=/api/v1/status/
+# must returned False if excluded_paths contains /api/v1/status/
 
 class Auth():
     """ Base class for the auth
     """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        # sourcery skip: assign-if-exp, boolean-if-exp-identity, remove-unnecessary-cast, simplify-empty-collection-comparison, use-named-expression
         """ require_auth """
-        return False
+        if path is None or excluded_paths is None or excluded_paths == []:
+            return True
+
+        # synthezie path
+        if path[-1] == '/':
+            path = path[:-1]
+
+        # Compile the regular expression pattern
+        pattern = '|'.join(re.escape(p.rstrip('/')) for p in excluded_paths)
+
+        # Compile the pattern into a regular expression object
+        pattern = '({})'.format(pattern)
+
+        match = re.search(pattern, path)
+
+        # if the path matches the excluded paths, return False
+        if match:
+            return False
+        else:
+            return True
 
     def authorization_header(self, request=None) -> str:
         """ authorization_header """
