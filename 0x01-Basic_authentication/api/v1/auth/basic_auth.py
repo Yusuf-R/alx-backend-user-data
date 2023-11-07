56becc9fc4e1acf40d2d237d871c88ea6a408f30
Yusuf-R <y.abdulwasiu@gmail.com>
2023-11-07 15:53:10 +0100

    feat: :sparkles: basis Base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -24,4 +24,28 @@ class BasicAuth(Auth):
     - None.
     """
 
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """
+        Extracts the base64 authorization header from the given string.
+
+        Args:
+            authorization_header (str): The authorization header string.
+
+        Returns:
+            str: The extracted base64 authorization header.
+        """
+        # Check if the authorization header is None
+        if authorization_header is None:
+            return None
+
+        # Check if the authorization header is of type string
+        if not isinstance(authorization_header, str):
+            return None
+
+        # Check if the authorization header starts with 'Basic '
+        if not authorization_header.startswith('Basic '):
+            return None
+
+        # Extract and return the base64 authorization header
+        return authorization_header.split(' ')[1]
