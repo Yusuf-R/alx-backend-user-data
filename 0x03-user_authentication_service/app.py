59ae290ef195332df9402b85bc7e775b96d60d2e
Yusuf-R <y.abdulwasiu@gmail.com>
2023-11-14 07:14:35 +0100

    fix: :bug: set exception to abort 403

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -173,7 +173,7 @@ def get_reset_password_token():
         user = AUTH._db.find_user_by(email=email)
         reset_token = AUTH.get_reset_password_token(email)
         return jsonify({"email": user.email, "reset_token": reset_token}), 200
-    except ValueError:
+    except Exception:
         abort(403)
 
 
